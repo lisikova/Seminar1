6f4c7e1ba3b0b6f98628705ea5a2a2ef1fa5d82a
Lysikova Galina <vereshinskaya2011@yandex.ru>
2022-08-12 00:19:28 +0700

    Первая задача

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("введите число 1: ");
+int number1 = int.Parse (Console.ReadLine ());
+Console.Write ("введите число 2: ");
+int number2 = int.Parse (Console.ReadLine ());
+if (number1 > number2)
+{
+Console.WriteLine ($"{number1} больше числа {number2}");
+}
+else 
+{
+Console.WriteLine ($"{number2} больше числа {number1}");
+}
